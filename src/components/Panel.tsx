e1e54c5cb5a8fd0a45e5b3c72bd9fbc2e5406d79
Simone Liu <simoneliu94@gmail.com>
2021-04-30 11:06:34 -0400

    Add wave header

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import masks from "../assets/img/WearAMask.png";
 
 class Panel extends React.Component {
 	render() {
 		return (
-			<div className="panel_container">
-				<div className="svg_container">
+			<div className="wave-container">
+				{/* <div className="svg_container">
 					<svg
 						viewBox="0 0 500 150"
 						preserveAspectRatio="none"
@@ -12,7 +13,16 @@ class Panel extends React.Component {
 					>
 						<path d="M-22.01,101.14 C149.99,150.00 271.44,-24.17 537.81,103.13 L500.00,0.00 L0.00,0.00 Z"></path>
 					</svg>
+					<div className="panel_img">
+						<img src={masks} alt="mask"></img>
+					</div>
+				</div> */}
+				<div className="panel_img">
+					<img src={masks} alt="mask"></img>
 				</div>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+					<path d="M0,288L48,250.7C96,213,192,139,288,144C384,149,480,235,576,224C672,213,768,107,864,80C960,53,1056,107,1152,138.7C1248,171,1344,181,1392,186.7L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+				</svg>
 			</div>
 		);
 	}
---
+++ webpack.config.ts
@@ -0,0 +1,68 @@
+import path from "path";
+import webpack from "webpack";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+
+const config: webpack.Configuration = {
+	entry: "./src/index.tsx",
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: [
+							"@babel/preset-env",
+							"@babel/preset-react",
+							"@babel/preset-typescript",
+						],
+					},
+				},
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				loader: [
+					"style-loader",
+					{
+						loader: "css-loader",
+						options: {
+							modules: true,
+						},
+					},
+					"sass-loader",
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							esModule: false,
+						},
+					},
+				],
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: "bundle.js",
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "build"),
+		compress: true,
+		port: 4000,
+	},
+	plugins: [
+		new ForkTsCheckerWebpackPlugin({
+			async: false,
+		}),
+	],
+};
+
+export default config;
